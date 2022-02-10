b9c6fa8c1be1e96c9e04b97da88bd398055f2ab4
manirabona walker <55847682+manirabona-programer@users.noreply.github.com>
2022-02-10 08:44:46 +0200

    add a comment on every function event

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -13,6 +13,10 @@ class PaymentController extends Controller
        $this->Payment_service = new ServicesPaymentService();
     }
 
+    /**
+     * the main index rendered every time user try to access the
+     * system
+     */
     public function ProcessPayment() {
        $netAmount = $this->Payment_service->setProductPrice(1600)->calculateNetAmmount();
        dd($netAmount);
--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -1,12 +1,12 @@
 <?php
     namespace App\Services;
     class PaymentService {
-        public $transaction_fees;
-        public $coupon_rate;
-        public $discount_rate;
-        public $vat;
-        public $product_price;
-        public $amount;
+        public $transaction_fees; // transaction fees (each transaction)
+        public $coupon_rate; // coupon rate as 2%
+        public $discount_rate; // membership discount rate as 3%
+        public $vat; // the VAT of product price as 18%
+        public $product_price; // a given price
+        public $amount; // the net amount after all calculation
 
         public function __construct() {
             $this->transaction_fees = config('Payment.transaction_fees');
@@ -15,11 +15,17 @@
             $this->vat = config('Payment.VAT');
         }
 
+        /**
+         * set the product price (given)
+         */
         public function setProductPrice($product_price){
             $this->product_price = $product_price;
             return $this;
         }
 
+        /**
+         * calculate the VAT on product price (given price)
+         */
         public function calculateVAT(){
             if($this->vat){
                 $this->amount += ($this->amount * $this->vat);
@@ -27,11 +33,18 @@
             return $this;
         }
 
+        /**
+         * calculate the coupon of the given price
+         */
         public function calculateCoupon($product_price){
             if(!$this->coupon_rate['enabled']) return $product_price;
             return $product_price - ($product_price * $this->coupon_rate['value']);
         }
 
+        /**
+         * calculate the net ammount of product price
+         * where every required fees removed
+         */
         public function calculateNetAmmount(){
             $this->amount = $this->calculateCoupon($this->product_price);
             $this->applyTransactionFees()->calculateDiscount();
@@ -39,11 +52,17 @@
             return $this->amount;
         }
 
+        /**
+         * apply transaction fees on each transaction
+         */
         public function applyTransactionFees(){
             $this->amount += $this->transaction_fees;
             return $this;
         }
 
+        /**
+         * calculate the discount of product price (if user exist in membership storage)
+         */
         public function calculateDiscount(){
             $this->amount -= ($this->amount * $this->discount_rate['value']);
             return $this;
